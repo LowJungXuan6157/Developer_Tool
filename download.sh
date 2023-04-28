61407b7022612b8fd1f33b321608332158dfd99a
LowJungXuan6157 <124644086+LowJungXuan6157@users.noreply.github.com>
2023-04-28 16:31:03 +0000

    shell file

---
+++ download.sh
@@ -0,0 +1,29 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$USER/.zprofile 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install --cask flutter
+brew install --cask android-studio
+brew install --cask postman
+brew install --cask visual-studio-code
+brew install --cask github
+brew install --cask monitorcontrol
+brew install --cask whatsapp
+brew install --cask figma
+brew install --cask zoom
+brew install --cask teamviewer
+brew install --cask microsoft-outlook
+brew install --cask skype
+brew install --cask jetbrains-toolbox
+brew install --cask anydesk
+brew install --cask notion
+brew install --cask slack
+brew install --cask google-chrome
+brew install --cask microsoft-excel
+brew install --cask microsoft-teams
+brew install --cask microsoft-word
+brew install --cask microsoft-powerpoint
+brew install java
+brew install node
+brew install git
+brew install cocoapods
+brew install firebase-cli
